02650397a0b8fafe390d0895469aab3756ea100d
Alex Leith <alexgleith@gmail.com>
2016-03-01 12:10:25 +1100

    Time series and getFeatureInfo working

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,6 @@
-const placesAPIKey = "AIzaSyDk66629MXhg6QKVEeKpaweURt7-sl9U-4";
+const 	server = "http://wms-master.tidetech.org",
+	    owsurl = server + '/geoserver/ows',
+	    tt_att = 'data &copy TideTech';
 
 $("#full-extent-btn").click(function() {
 	map.fitBounds(boroughs.getBounds());
@@ -43,16 +45,14 @@ var baseMaps = {
 };
 
 var center = new L.LatLng(20.38582, 29.35546),
-	startZoom = 3,
-	server = "http://wms.tidetech.org",
-	tt_att = "data &copy TideTech";
+	startZoom = 3;
 
 var map = L.map("map", {
 	zoom: startZoom,
 	center: center,
 	layers: [gray],
 	zoomControl: true,
-	attributionControl: true
+	attributionControl: false
 });
 
 //Locate control
@@ -64,9 +64,12 @@ var lc = L.control.locate({
 }).addTo(map);
 map.on('dragstart', lc._stopFollowing, lc);
 
+//Layer control
+L.control.layers(baseMaps, {}).addTo(map);
+
 //Google Places Autocomplete
 var input = /** @type {!HTMLInputElement} */(
-      document.getElementById('searchbox'));
+    document.getElementById('searchbox'));
 var autocomplete = new google.maps.places.Autocomplete(input);
 autocomplete.addListener('place_changed', function() {
 	var place = autocomplete.getPlace();
@@ -86,3 +89,151 @@ autocomplete.addListener('place_changed', function() {
 		console.log(place.geometry)
 	}
 });
+
+//Do the mappy stuff part two. Find some layers and map them.
+var clickMarker;
+var currentLayer = "tidetech:air_temperature_degC";
+var currentLayerTitle = "Air Temperature (Celcius)";
+
+function handleJson(data, coords) {
+    var text = '<div class="table-responsive"><table class="table table-condensed">',
+    	u = null,
+    	v = null;
+	text = text + '<tr><th>Attribute</th><th>Value</th></tr>'
+    var thisFeatureProperties = data.features[0].properties;
+    for (var i = data.features.length - 1; i >= 0; i--) {
+        thisFeatureProperties = data.features[i].properties;
+        Object.keys(thisFeatureProperties).forEach(function(key,index) {
+            if (key === 'u') {
+                u = thisFeatureProperties[key];
+            } else if (key === 'v') {
+                v = thisFeatureProperties[key];
+            } else {
+                text = text + "<tr><td>" + key.split(':')[0] + "</td><td>" + thisFeatureProperties[key].toFixed(2) + '</td>';
+            }
+        });
+    };
+    if(u && v) {
+        var dir_rads = Math.atan2(u,v);
+        if(dir_rads < 0) {dir_rads = dir_rads + (2 * Math.PI);}
+        dir_deg = dir_rads * (180/Math.PI)
+
+        text = text + "<b>direction:</b> " + dir_deg.toFixed(0) + '<br>';
+    }
+    text = text + '</table></div>'
+    
+    clickMarker = new L.marker(coords).addTo(map).bindPopup(text).openPopup();
+}
+
+map.on('click', function(e) {
+    if (clickMarker) {
+        map.removeLayer(clickMarker);
+    };
+    
+    var queryCoordinates = e.latlng,
+    	lng = queryCoordinates.lng,
+    	lat = queryCoordinates.lat;
+    
+    var parameters = {
+        service : 'WMS',
+        version : '1.1.1',
+        request : 'GetFeatureInfo',
+        layers : currentLayer,
+        query_layers : currentLayer,
+        feature_count : 10,
+        info_format : 'text/javascript',
+        format_options : 'callback:getJson',
+        SrsName : 'EPSG:4326',
+        width: 101,
+        height: 101,
+        x: 50,
+        y: 50,
+        bbox: (lng - 0.1) + "," + (lat - 0.1) + "," + (lng + 0.1) + "," + (lat + 0.1)
+    };
+    var url = owsurl + L.Util.getParamString(parameters)
+    //console.log(url);
+    $.ajax({
+        url : owsurl + L.Util.getParamString(parameters),
+        dataType : 'jsonp',
+        jsonpCallback : 'getJson',
+        success : function(data) {
+            handleJson(data, queryCoordinates);
+        }
+    });
+});
+
+function pad(num, size) {
+    var s = num+"";
+    while (s.length < size) s = "0" + s;
+    return s;
+}
+
+Date.prototype.addDays = function(days) {
+    var dat = new Date();
+    dat.setDate(dat.getDate() + days);
+    return dat;
+}
+
+var today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth()+1;
+var yyyy = today.getFullYear();
+
+
+var later = today.addDays(14);
+var dd2 = later.getDate();
+var mm2 = later.getMonth()+1;
+var yyyy2 = later.getFullYear();
+
+var startTime = yyyy+'-'+pad(mm,2)+'-'+pad(dd,2)+'T18:00:00.000';
+var endTime = yyyy2+'-'+pad(mm2,2)+'-'+pad(dd2,2)+'T18:00:00.000';
+var timeStep = 60*60*6;
+
+var overlay = L.WMS.overlay(owsurl, {
+    'layers': currentLayer,
+    'transparent': true,
+    'opacity': 0.4
+});
+overlay.addTo(map);
+
+//Initialize the SliderControl with the WMS layer, a start time, an end time, and time step
+var sliderControl = L.control.sliderControl({position: 'bottomright', layer: overlay, startTime: startTime, endTime: endTime, timeStep: timeStep});
+
+// Add the slider to the map
+map.addControl(sliderControl);
+
+// Start the slider
+sliderControl.startSlider();
+
+var sliderControl = L.control.sliderControl({position: 'topright', layer: overlay, startTime: startTime, endTime: endTime, timeStep: timeStep*1000});
+
+
+/*
+$.ajax({
+  type: "GET",
+  url: owsurl + "?SERVICE=WMS&request=getcapabilities",
+  dataType: "xml",
+  success: parseXml
+});
+
+function parseXml(xml) {
+  var layerIndex = 0
+  $(xml).find("Layer").find("Layer").each(function() {
+    var title = $(this).find("Title").first().text();
+    var name = $(this).find("Name").first().text();
+    if(name === 'tidetech:air_temperature') {
+      var time = $(this).find("Dimension");
+      console.log(time.text());
+    }
+    //Check for layer groups
+    var patt = new RegExp("Group");
+    var res = patt.test(title);
+    if(!res) {
+      featureLayers.push(name)
+      featureLayersName.push(title)
+    }
+  });
+}
+*/
+
+
